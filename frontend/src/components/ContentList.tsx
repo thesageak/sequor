bc56f6527f691986d91536d6f75ea586f1b9941d
thesageak <sageaguinakang@gmail.com>
2025-12-03 04:03:54 -0800

    Add Content listing on MediaDetailsLayout

--- frontend/src/components/ContentList.tsx
+++ frontend/src/components/ContentList.tsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 interface WatchableMediaProps {
     title?: string;
     num: number;
 }
 
-export default function ContentList({ title, num }: WatchableMediaProps) {
+export default function ContentList({title="", num }: WatchableMediaProps) {
+    const [isMarked, setIsMarked] = useState(false);
+    
     return (
-        <div className="flex width-full h-10 bg-seqGray">
-            <button> eye </button>
-            <p>Chapter {num} - {title}</p>
+        <div className={`flex justify-start items-center w-full h-12 my-3 bg-seqDarkGray border-4 border-seqDarkGray ${isMarked ? "" : "border-l-seqBlue"} `}>
+            <button 
+                className="z-20 m-5 p-1 bg-gray-700 hover:bg-amber-100"
+                onClick={() => setIsMarked(prev => !prev)}
+            > 
+                eye
+            </button>
+            <p className="text-[1.5rem] font-bold -translate-y-0.5">
+                Chapter {num} {title}
+            </p>
         </div>
     )
 }
--- frontend/src/layouts/MediaDetailsLayout.tsx
+++ frontend/src/layouts/MediaDetailsLayout.tsx
@@ -5,11 +5,13 @@ import type { MangaResult } from '../pages/search/SearchTypes';
 import ContentList from '../components/ContentList.tsx'
 
 // Chainsaw Man Manga ID: 116778
+// JoJo Part 7: 1706
 
 export default function MediaDetailsLayout() {
     const { id } = useParams();
     console.log('ID param:', id);
     const [results, setResults] = useState<MangaResult>();
+
 
     useEffect(() => {
         async function fetchResults() {
@@ -17,6 +19,7 @@ export default function MediaDetailsLayout() {
                 const response = await axios.get(`http://localhost:3000/api/manga/${id}`);
                 console.log(response);
                 setResults(response.data);
+                console.log(response.data.chapter);
             } catch (error) {
                 console.error('Failed to fetch media', error);
             }
@@ -34,14 +37,14 @@ export default function MediaDetailsLayout() {
                 <div className="absolute inset-0 backdrop-blur-sm" />
             </div>
 
-            <div className="flex absolute mx-auto inset-0 max-w-7xl m-25">
+            <div className="flex absolute mx-auto inset-0 max-w-7xl m-25 sm:px-25">
                 <div className="flex justify-center top-25">
                     <img
                         src={results?.image_url}
                         className="w-50 h-76"
                     />
 
-                    <div className="flex flex-col px-4">
+                    <div className="flex flex-col px-5">
                         <h1 className="text-white font-bold text-[2.5rem] text-shadow-lg">
                             {results?.title || results?.title_english || results?.title_japanese}
                         </h1>
@@ -60,13 +63,18 @@ export default function MediaDetailsLayout() {
                 </div>
             </div>
 
-            <div className="max-w-7xl mx-auto">
+            <div className="max-w-7xl mx-auto sm:px-25">
                 <p className="flex-1 mt-20">
                     {results?.synopsis}
                 </p>
                 <h2 className="flex-1 text-[3rem] font-bold">
                     Chapters
                 </h2>
+                {
+                    Array.from({ length: results?.chapters || 0 }, (_, i) => i + 1).reverse().map((chapter) => (
+                        <ContentList num={chapter} />
+                    ))
+                }
             </div>
 
         </div>
